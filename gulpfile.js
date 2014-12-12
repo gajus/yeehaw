1f01304012cf7da27b669649923015120c564472
Gajus Kuizinas <gk@anuary.com>
2014-12-12 13:57:08 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp'),
+    mocha = require('gulp-mocha'),
+    jshint = require('gulp-jshint'),
+    jsonfile = require('jsonfile');
+
+gulp.task('lint', function () {
+    return gulp
+        .src('./src/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('test', ['lint'], function () {
+    return gulp
+        .src(['./tests/*.js'], {read: false})
+        .pipe(mocha());
+});
+
+gulp.task('watch', function () {
+    gulp.watch(['./src/*', './tests/*'], ['default']);
+});
+
+gulp.task('default', ['version']);
---
+++ src/yeehaw.js
@@ -0,0 +1,3 @@
+var Yeehaw;
+
+module.exports = Yeehaw;
